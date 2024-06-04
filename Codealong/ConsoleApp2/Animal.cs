9b4615cfc0ee1cea709a495ffc5010487816567a
Imlov3code <7042487@qq.com>
2024-06-04 13:21:01 +0200

    add codealong

---
+++ Codealong/ConsoleApp2/Animal.cs
@@ -0,0 +1,95 @@
+﻿namespace AnimalApp
+{
+    internal class AnimalData
+    {
+        public string Name { get; set; }
+        public string Art { get; set; }
+        public int Age { get; set; }
+
+        public AnimalData(string name, string art, int age)
+        {
+            Name = name;
+            Art = art;
+            Age = age;
+        }
+
+        string GetName()
+        {
+            return Name;
+        }
+
+        string GetArt()
+        {
+            return Art;
+        }
+
+        int GetAge()
+        {
+            return Age;
+        }
+
+        public void PrintOutAnimal()
+        {
+            Console.WriteLine($"Name: {Name} Art: {Art} Age: {Age}");
+        }
+    }
+
+    class AnimalManager
+    {
+        public void Addanimal(List<AnimalData> animals)
+        {
+            string name = Console.ReadLine();
+            string art = Console.ReadLine();
+
+            int age;
+
+            while (!int.TryParse(Console.ReadLine(), out age))
+            {
+                Console.WriteLine("invalid input for age");
+            }
+
+            AnimalData animal = new AnimalData(name, art, age);
+            animals.Add(animal);
+
+            Console.WriteLine("Animal added successfully");
+        }
+
+        public void ShowAllAnimals(List<AnimalData> animals)
+        {
+            if (animals.Count == 0)
+            {
+                Console.WriteLine("No animals");
+                return;
+            }
+
+            Console.WriteLine("Animals:");
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal.Name);
+                Console.WriteLine(animal.Art);
+                Console.WriteLine(animal.Age);
+            }
+        }
+
+        public void SearchArt(List<AnimalData> animals)
+        {
+            Console.WriteLine("Enter the art to search for: ");
+            string art = Console.ReadLine();
+
+            var filtered_animals = animals.Where(a => a.Art.Equals(art, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            if (filtered_animals.Count == 0)
+            {
+                Console.WriteLine($"No animals found for Art '{art}':");
+                return;
+            }
+
+            Console.WriteLine($"Animal of Art {art} : ");
+            foreach (var animal in filtered_animals)
+            {
+                Console.WriteLine(animal.Art);
+            }
+        }
+    }
+}
---
+++ Codealong/ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace AnimalApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<AnimalData> animals = new List<AnimalData>();
+           AnimalManager manager = new AnimalManager();
+
+           while (true)
+           {
+               Console.WriteLine("\nMenu");
+               Console.WriteLine("1. Add a new animals");
+               Console.WriteLine("2. Show all animals");
+               Console.WriteLine("3. Search by Art");
+
+               int choice;
+
+               if (!int.TryParse(Console.ReadLine(), out choice))
+               {
+                   Console.WriteLine("invalid choice");
+                   continue;
+               }
+
+               switch (choice)
+               {
+                    case 1:
+                        manager.Addanimal(animals);
+                        break;
+
+                    case 2:
+                        manager.ShowAllAnimals(animals);
+                        break;
+
+                    case 3:
+                        manager.SearchArt(animals);
+                        break;
+
+               }
+           }
+        }
+    }
+}
